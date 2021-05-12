ac5e5b69f27a654fce14af0ae1c171cbd3ca8d13
BA7JCM <1677128936@qq.com>
2021-05-12 21:54:57 +0800

    注释掉原有的get传值方式，改为post方式传值

--- auto.php
+++ auto.php
@@ -1,13 +1,33 @@
 <?PHP
 include 'connect.php';
+function send_post($url, $post_data) {
+    $data = http_build_query($post_data);
+    $options = array(
+        'http' => array(
+            'method' => 'POST',
+            // 'header' => 'Content-type:application/x-www-form-urlencoded',
+            'content' => $data,
+            // 'timeout' => 15 * 60 // 超时时间（单位:s）
+        )
+    );
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+ 
+    return $result;
+}
 $sql = "SELECT * FROM list";
 $list = mysqli_query($connect,$sql);
 while($run = mysqli_fetch_assoc($list)){
     $account = $run['tel'];
     $password = $run['password'];
     $name = $run['name'];
-    $url = "https://这里换成你自己的部署地址/main.php?account=$account&password=$password";
-    $single = file_get_contents($url);
+    // $url = "https://这里换成你自己的部署地址/main.php?account=$account&password=$password";
+    // $single = file_get_contents($url);
+    $post_data = array(
+        'account' => $account,
+        'password' => $password
+    );
+    $single= send_post('https://这里换成你自己的部署地址/main.php', $post_data);
     if($single){
         echo $single;
         echo "<br>";
